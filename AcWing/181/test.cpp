a75e941231ef25b51a60f59ebff57e151f1040f3
LroseC <1441276292@qq.com>
2021-04-13 19:39:38 +0800

    ...

--- AcWing/181/test.cpp
+++ AcWing/181/test.cpp
@@ -1,5 +1,7 @@
 #include <cstdio>
+#include <string>
 #include <cstring>
+#include <iostream>
 
 using namespace std;
 
@@ -9,8 +11,28 @@ const int N = 24;
 
 int T;
 int center[] = {6, 7, 8, 11, 12, 15, 16, 17};
+int op[8][7] = {
+	{0, 2, 6, 11, 15, 20, 22},
+	{1, 3, 8, 12, 17, 21, 23},
+	{10, 9, 8, 7, 6, 5, 4},
+	{19, 18, 17, 16, 15, 14, 13},
+	{23, 21, 17, 12, 8, 3, 1},
+	{22, 20, 15, 11, 6, 2, 0},
+	{13, 14, 15, 16, 17, 18, 19},
+	{4, 5, 6, 7, 8, 9, 10}
+};
+/*
+      0     1
+      2     3
+4  5  6  7  8  9  10
+      11    12
+13 14 15 16 17 18 19
+      20    21
+      22    23
+*/
 int sum[4];
 int a[N], b[N];
+string ans;
 
 inline int max(int, int);
 
@@ -20,23 +42,25 @@ namespace IDA_star
 	void Work(int);
 	bool DFS(int, int);
 }
+void Print(void);
 
 int main()
 {
-	scanf("%d", &T);
-	while (T--) {
-		for (int i = 0; i < 24; ++i) scanf("%d", &a[i]);
+	while (1) {
+		scanf("%d", &a[0]);
+		if (!a[0]) break;
+		for (int i = 1; i < 24; ++i) scanf("%d", &a[i]);
 
-		if (IDA_star::DFS(0, 0)) {
-			puts("No moves needed");
-			continue;
-		}
 		int dep = 0;
 		while (!IDA_star::DFS(dep, 0)) ++dep;
 		if (!dep) {
 			puts("No moves needed");
+			cout << a[center[0]] << endl;
 			continue;
 		}
+		cout << ans << endl;
+		cout << a[center[0]] << endl;
+		ans.clear();
 	}
 
 	return 0;
@@ -55,10 +79,14 @@ int IDA_star::f(void)
 }
 void IDA_star::Work(int x)
 {
+	int t = a[op[x][0]];
+	for (int i = 0; i < 6; ++i) a[op[x][i]] = a[op[x][i + 1]];
+	a[op[x][6]] = t;
 	return;
 }
 bool IDA_star::DFS(int lim, int dep)
 {
+	int b[24];
 	using namespace IDA_star;
 	if (dep > lim) return 0;
 	int fi = f();
@@ -66,11 +94,31 @@ bool IDA_star::DFS(int lim, int dep)
 	if (fi + dep > lim) return 0;
 	for (int i = 0; i < 8; ++i) {
 		memcpy(b, a, sizeof b);
+		ans.push_back(i + 'A');
 		Work(i);
 		if(DFS(lim, dep + 1)) return 1;
 		memcpy(a, b, sizeof a);
+		ans.pop_back();
 	}
 	return 0;
+}
+void Print(void)
+{
+	int idx = 0;
+	for (int r = 0; r < 7; ++r) {
+		if (r == 2 || r == 4) {
+			for (int j = 0; j < 7; ++j) printf("%3d", a[idx++]);
+			putchar('\n');
+		}
+		else {
+			for (int i = 0; i < 6; ++i) putchar(' ');
+			printf("%3d", a[idx++]);
+			for (int i = 0; i < 3; ++i) putchar(' ');
+			printf("%3d", a[idx++]);
+			putchar('\n');
+		}
+	}
+	return;
 }
 /*
       0     1
