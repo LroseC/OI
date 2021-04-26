64fbedfcfdf851a2ebe052913eaa03a91d3a65e8
LroseC <1441276292@qq.com>
2021-04-26 21:09:44 +0800

    ...

--- AcWing/184/test.cpp
+++ AcWing/184/test.cpp
@@ -5,29 +5,67 @@ using namespace std;
 
 #define reg register
 #define ll long long
-const int N = 26;
+const int N = 30;
 
-int n;
-char f[3][N];
-int p[N];
-bool vis[N];
+int n, idx;
+char e[3][N];
+int path[N], q[N];
+bool st[N];
 
-void Init(void);
-void DFS(int c, int k);
+bool check(void);
+bool dfs(int);
 
 int main()
 {
 	scanf("%d", &n);
-	for (int i = 0; i < 3; ++i)
-		for (int j = 0; j < n; ++j) cin >> f[i][j];
-	DFS(n - 1);
-	for (int i = 0; i < n; ++i) cout << p[i] << ' ';
+	for (int i = 0; i < 3; ++i) scanf("%s", e[i]);
+	for (int i = n - 1; i >= 0; --i)
+		for (int j = 0; j < 3; ++j)
+			if (!st[e[j][i] - 'A']) {
+				st[e[j][i] - 'A'] = 1;
+				q[idx++] = e[j][i] - 'A';
+			}
+
+	memset(st, 0, sizeof st);
+	memset(path, 0xff, sizeof path);
+
+	dfs(0);
+
+	for (int i = 0; i < n; ++i) printf("%d%c", path[i], i == n - 1 ? '\n' : ' ');
+
 	return 0;
 }
-void Init(void)
+bool dfs(int dep)
 {
-	memset(0xff, p, sizeof p);
+	if (dep == n) return 1;
+	for (int i = 0; i < n; ++i)
+		if (!st[i]) {
+			st[i] = 1;
+			path[q[dep]] = i;
+			if (check() && dfs(dep + 1)) return 1;
+			st[i] = 0;
+			path[q[dep]] = -1;
+		}
+
+	return 0;
 }
-void DFS(int c, int k)
+bool check(void)
 {
-	if (~p[f[k][c]]) 
+	for (int i = n - 1, t = 0; i >= 0; --i) {
+		int a = e[0][i] - 'A', b = e[1][i] - 'A', c = e[2][i] - 'A';
+		if (~path[a] && ~path[b] && ~path[c]) {
+			a = path[a], b = path[b], c = path[c];
+			if (~t) {
+				if ((a + b + t) % n != c) return 0;
+				if (!i && a + b + t >= n) return 0;
+				t = (a + b + t) / n;
+			}
+			else {
+				if ((a + b + 1) % n != c && (a + b) % n != c) return 0;
+				if (!i && a + b >= n) return 0;
+			}
+		}
+		else t = -1;
+	}
+	return 1;
+}
