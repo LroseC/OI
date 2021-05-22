86fbfafd49408e4871bfe7bc4af1e75a7b2ae99a
LroseC <1441276292@qq.com>
2021-05-22 09:18:32 +0000

    ...

--- AcWing/239/main.cpp
+++ AcWing/239/main.cpp
@@ -2,10 +2,17 @@
 
 using namespace std;
 
+const int M = 1e4 + 10, N = M << 1;
+
+struct PII
+{
+	int 
+
 int n, m;
+int l, r;
+char str[10];
 
 int main()
 {
 	scanf("%d%d" ,&n, &m);
-	while (m--) {
-		scanf("%d%d
+	for (int i = 1; i <= m; ++i) {
