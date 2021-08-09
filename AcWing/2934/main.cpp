50be73b439e5bca66e23d1312594662d85f73cf4
LroseC <1441276292@qq.com>
2021-08-09 09:35:51 +0800

    AcWing_2934

--- AcWing/2934/main.cpp
+++ AcWing/2934/main.cpp
@@ -29,12 +29,12 @@ void insert(int cur, int state, LL w)
 		h[cur][t] = state;
 		v[cur][t] = w;
 	}
-	v[cur][t] += w;
+	else v[cur][t] += w;
 }
 
 int get(int state, int k)
 {
-	return ((state >> k * 2) & 3);
+	return state >> k * 2 & 3;
 }
 int set(int v, int k)
 {
@@ -44,15 +44,15 @@ int set(int v, int k)
 int main(void)
 {
 	cin >> n >> m;
-	for (int i = 1; i <= n; ++i)
-		for (int j = 1; j <= m; ++j) {
-			char ch; cin >> ch;
-			if (ch == '.') {
+	for (int i = 1; i <= n; ++i) {
+		char str[20];
+		scanf("%s", str + 1);
+		for (int j = 1; j <= m; ++j)
+			if (str[j] == '.') {
 				g[i][j] = 1;
-				edx = i;
-				edy = j;
+				edx = i, edy = j;
 			}
-		}
+	}
 
 	LL res = 0;	
 	memset(h, -1, sizeof h);
@@ -65,51 +65,59 @@ int main(void)
 		for (int j = 1; j <= m; ++j) {
 			int last = cur;
 			cur ^= 1;
+			cnt[cur] = 0;
 			memset(h[cur], -1, sizeof h[cur]);
 			for (int k = 1; k <= cnt[last]; ++k) {
 				int state = h[last][q[last][k]];
-				int w = v[last][state];
+				LL w = v[last][q[last][k]];
 				int a = get(state, j - 1), b = get(state, j);
-				if (!g[i][j] && (!a && !b))
+				if (!g[i][j] && (!a && !b)) {
 					insert(cur, state, w);
-				else if (!a && !b)
-					insert(cur, state + set(1, j - 1) + set(2, j), w);
+				}
+				else if (!a && !b) {
+					if (g[i + 1][j] && g[i][j + 1])
+						insert(cur, state + set(1, j - 1) + set(2, j), w);
+				}
 				else if (!a && b) {
-					insert(cur, state - set(b, j) + set(b, j - 1), w);
-					insert(cur, state, w);
+					if (g[i + 1][j]) insert(cur, state - set(b, j) + set(b, j - 1), w);
+					if (g[i][j + 1]) insert(cur, state, w);
 				}
 				else if (a && !b) {
-					insert(cur, state, w);
-					insert(cur, state - set(a, j - 1) + set(a, j), w);
+					if (g[i + 1][j]) insert(cur, state, w);
+					if (g[i][j + 1]) insert(cur, state - set(a, j - 1) + set(a, j), w);
 				}
 				else if (a == 1 && b == 1) {
-					for (int s = 1, l = j + 1; l <= m; ++l) {
+					for (int s = 1, l = j + 1; ; ++l) {
 						int tmp = get(state, l);
-						if (tmp == 2)
+						if (tmp == 2) {
 							if (--s == 0) {
 								insert(cur, state - set(1, l) - set(a, j - 1) - set(b, j), w);
 								break;
 							}
-						else ++s;
+						}
+						else if (tmp == 1) ++s;
 					}
 				}
 				else if (a == 2 && b == 2) {
-					for (int s = 1, r = j - 2; r; --r) {
+					for (int s = 1, r = j - 2; ; --r) {
 						int tmp = get(state, r);
-						if (tmp == 1)
+						if (tmp == 1) {
 							if (--s == 0) {
 								insert(cur, state + set(1, r) - set(a, j - 1) - set(b, j), w);
 								break;
 							}
-							else ++s;
+						}
+						else if (tmp == 2) ++s;
 					}
 				}
 				else if (a == 2 && b == 1) {
-					insert(cur, state, w);
+					insert(cur, state - set(a, j - 1) - set(b, j), w);
 				}
-				else if (i == edx && j == edy)
+				else if (i == edx && j == edy) {
 					res += w;
+				}
 			}
 		}
 	}
+	printf("%lld\n", res);
 }
