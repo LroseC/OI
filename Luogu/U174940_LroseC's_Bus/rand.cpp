98243a7ca4530323880c603becfa563da0c2d9f0
LroseC <1441276292@qq.com>
2021-08-19 21:40:03 +0800

    ...

--- Luogu/U174940_LroseC's_Bus/rand.cpp
+++ Luogu/U174940_LroseC's_Bus/rand.cpp
@@ -1,9 +1,17 @@
 #include <random>
+#include <climits>
 #include <iostream>
 
 using namespace std;
 
 int n, m;
+
+mt19937 rd(time(NULL));
+
+int myrand(int l, int r)
+{
+	return 1.0 * rd() / UINT_MAX * (r - l + 1) + l + 0.5;
+}
 
 int main(void)
 {
@@ -12,7 +20,7 @@ int main(void)
 	cout << n << endl;
 	for (int i = 1; i <= 10; ++i) {
 		int u = myrand(1, 20), v = myrand(1, 20);
-		int tx = myrand(1, 10), ty = myrand(1, 10);
+		int tx = myrand(1, 9), ty = myrand(tx + 1, 10);
 		cout << u << ' ' << v << ' ' << tx << ' ' << ty << endl;
 	}
 	cout << m << endl;
