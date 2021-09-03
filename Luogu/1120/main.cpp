03b1c9d1ff98fac3b083c575548cd2f93ab6d8d6
LroseC <1441276292@qq.com>
2021-09-03 09:04:00 +0800

    Luogu_1120

--- Luogu/1120/main.cpp
+++ Luogu/1120/main.cpp
@@ -1,10 +1,13 @@
 #include <vector>
 #include <cstdio>
 #include <cctype>
+#include <cstring>
 #include <algorithm>
 #include <functional>
 
 using namespace std;
+
+typedef long long ll;
 
 template<typename Int>inline void read(Int &res)
 {
@@ -20,23 +23,50 @@ template<typename Int>inline void write(Int x)
 	while (p >= 0) { putchar(buf[p]); --p; }
 }
 
-int n;
-vector<int> stick;
+int n,m,len,ans;
+int a[100010];
+bool vis[100010];
+ll sum;
 
-bool dfs(int len, int x)
+
+bool dfs(int now, int last, int depth)
 {
+	if (depth == m) return 1;
+	if (now == len) return dfs(0, 1, depth + 1);
+	int fail = 0;
+	for (int i = last; i <= n; ++i) {
+		if (!vis[i] && now + a[i] <= len && fail != a[i]) {
+			vis[i] = 1;
+			if (dfs(now + a[i], i + 1, depth)) return 1;
+			fail = a[i];
+			vis[i] = 0;
+			if (now == 0 || now + a[i] == len) return 0;
+		}
+	}
+	return 0;
 }
-
-int main(void)
+int main()
 {
 	read(n);
-	for (int i = 1; i <= n; ++i) {
-		int x; read(x);
-		if (x <= 50) stick.emplace_back(x);
+	sum = 0;
+	for (int i = 1; i <= n; ++i){
+		read(a[i]);
+		if (a[i] > 50) {
+			--i, --n;
+			continue;
+		}
+		sum += a[i];
 	}
-	sort(stick.begin(), stick.end(), greater<int>());
-	int len = stick[0];
-	while (!dfs(len, 0)) ++len;
-	write(len); putchar('\n');
+	ans = sum;
+	sort(a + 1, a + 1 + n, greater<int>());
+	for (len = 1;len < sum; ++len) {
+		if (sum % len) continue;
+		m = sum / len;
+		if (dfs(0,0,0)) {
+			ans = len;
+			break;
+		}
+	}
+	printf("%d\n",ans);
 	return 0;
 }
