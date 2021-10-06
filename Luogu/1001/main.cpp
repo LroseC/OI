c2d6f9c0a496cc7f4895df34e0225e28e2083887
LroseC <1441276292@qq.com>
2021-10-06 14:06:29 +0800

    Luogu_1001

---
+++ Luogu/1001/main.cpp
@@ -0,0 +1,27 @@
+#include <cctype>
+#include <cstdio>
+
+using namespace std;
+
+inline int read(void)
+{
+	int res = 0, f = 1; char ch = getchar();
+	while (!isdigit(ch)) { if (ch == '-') f = -1; ch = getchar(); }
+	while (isdigit(ch)) { res = res * 10 + ch - '0'; ch = getchar(); }
+	return res * f;
+}
+template<typename T>
+inline void write(T x, char end = '\n')
+{
+	static char buf[50]; int p = -1;
+	if (x == 0) putchar('0');
+	if (x < 0) { putchar('-'); x = -x; }
+	while (x) { buf[++p] = x % 10; x /= 10; }
+	while (~p) { putchar(buf[p] + '0'); --p; } putchar(end);
+}
+
+int main(void)
+{
+	write(read() + read());
+	return 0;
+}
