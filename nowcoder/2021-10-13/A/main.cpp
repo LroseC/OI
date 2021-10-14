538a0155f1c6040f3f32614aa4162347035edd53
LroseC <1441276292@qq.com>
2021-10-14 17:30:52 +0800

    ...

--- nowcoder/2021-10-13/A/main.cpp
+++ nowcoder/2021-10-13/A/main.cpp
@@ -2,7 +2,8 @@
 
 using namespace std;
 
-const int N = 1e5 + 10;
+const int N = 1e6 + 10;
+typedef pair<int, int> PII;
 
 template<typename T = int>
 inline T read(void)
@@ -25,8 +26,10 @@ inline void write(T x, char end = '\n')
 int a[N];
 int b[N];
 int cnt[N];
+int ans[N];
 bool vis[N];
 vector<int> nums;
+priority_queue<PII> heap;
 
 inline bool cmp(int a, int b)
 {
@@ -35,76 +38,86 @@ inline bool cmp(int a, int b)
 
 inline int init(void)
 {
-	memset(a, 0, sizeof a);
-	memset(b, 0, sizeof b);
-	memset(cnt, 0, sizeof cnt);
-	memset(vis, 0, sizeof vis);
-	nums.clear();
 	return 0;
 }
 inline int work(void)
 {
 	int n = read(), x = read();
+	nums.clear();
+	memset(b, 0, n + 1 << 2);
+	memset(cnt, 0, n + 1 << 2);
+	memset(ans, 0, n + 1 << 2);
+	while (heap.size()) heap.pop();
+	memset(vis, 0, n + 1 * sizeof(bool));
 	for (int i = 1; i <= n; ++i) a[i] = read();
 	if (x < 0) {
 		puts("yes");
 		sort(a + 1, a + 1 + n);
-		for (int i = 1; i <= n; ++i) write(a[i], i == n ? '\n' : ' ');
+		for (int i = 1; i <= n; ++i) write(a[i], ' ');
+		putchar('\n');
 	}
 	else if (x == 0) {
-		nums.clear();
-		for (int i = 1; i <= n; ++i) nums.emplace_back(a[i]);
-		sort(nums.begin(), nums.end());
-		nums.erase(unique(nums.begin(), nums.end()), nums.end());
-		memset(cnt, 0, sizeof cnt);
+		PII tmp;
+		set<PII> S;
+		sort(a + 1, a + 1 + n);
+		for (int i = 1; i <= n; ++i) {
+			if (i == 1) tmp = {1, a[i]};
+			else {
+				if (a[i] == a[i - 1]) ++tmp.first;
+				else {
+					S.insert(tmp);
+					tmp = {1, a[i]};
+				}
+			}
+		}
+		S.insert(tmp);
+
+		for (int i = 1; i <= n; ++i) {
+			auto it = S.end(); --it;
+			if (i > 1 && it->second == a[i - 1] && it != S.begin()) {
+				--it;
+				auto tmp = *it; S.erase(it);
+				a[i] = tmp.second;
+				--tmp.first;
+				if (tmp.first) S.insert(tmp);
+			}
+			else {
+				auto tmp = *it; S.erase(it);
+				a[i] = tmp.second;
+				--tmp.first;
+				if (tmp.first) S.insert(tmp);
+			}
+		}
+		bool fail = 0;
 		for (int i = 1; i <= n; ++i)
-			++cnt[lower_bound(nums.begin(), nums.end(), a[i]) - nums.begin()];
-		for (int i = 0; i < nums.size(); ++i) b[i] = i;
-		sort(b, b + nums.size(), cmp);
-		if (cnt[b[0]] > n - cnt[b[0]] + 1) puts("no");
+			if (a[i] - a[i - 1] == x) {
+				fail = 1;
+				break;
+			}
+		if (fail) puts("no");
 		else {
 			puts("yes");
-			for (int i = 0, j = 1; i < nums.size(); ++i) {
-				if (!cnt[b[i]])
-					continue;
-				if (i == j) ++j;
-				while (cnt[b[i]] > 1) {
-					while (!cnt[b[j]]) ++j;
-					--cnt[b[i]];
-					--cnt[b[j]];
-					write(nums[b[i]], ' ');
-					write(nums[b[j]], ' ');
-				}
-				--cnt[b[i]];
-				write(nums[b[i]], ' ');
-			}
+			for (int i = 1; i <= n; ++i) write(a[i], ' ');
 			putchar('\n');
 		}
 	}
 	else {
 		sort(a + 1, a + 1 + n, greater<int>());
-		if (x != a[1]) {
-			puts("yes");
-			for (int i = 1; i <= n; ++i) write(a[i], i == n ? '\n' : ' ');
-		}
-		else {
-			bool sec = 0;
-			int special = -1;
-			for (int i = 1; i <= n; ++i)
-				if (a[i] != x && a[i] != 0) {
-					sec = 1;
-					special = i;
-					break;
-				}
-			if (!sec) puts("no");
-			else {
-				puts("yes");
-				write(a[special], ' ');
-				for (int i = 1; i <= n; ++i)
-					if (i != special)
-						write(a[i], ' ');
-				putchar('\n');
+		bool sec = 0;
+		int special = -1;
+		for (int i = 1; i <= n; ++i)
+			if (a[i] != x && a[i] != 0) {
+				special = i;
+				break;
 			}
+		if (special == -1) puts("no");
+		else {
+			puts("yes");
+			write(a[special], ' ');
+			for (int i = 1; i <= n; ++i)
+				if (i != special)
+					write(a[i], ' ');
+			putchar('\n');
 		}
 	}
 	return 0;
