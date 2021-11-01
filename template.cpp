6a6ff79d2c8566a1b188cc4fc3581924ba9701ae
LroseC <1441276292@qq.com>
2021-11-01 18:36:40 +0800

    Update template file

---
+++ template.cpp
@@ -0,0 +1,45 @@
+#include <cctype>
+#include <cstdio>
+
+using namespace std;
+using LL = long long;
+
+struct FastStreamInput
+{
+	template<typename T>
+	FastStreamInput& operator>>(T &res)
+	{
+		res = 0; T f = 1; char ch = getchar();
+		while (!isdigit(ch)) { if (ch == '-') f = -1; ch = getchar(); }
+		while (isdigit(ch)) { res = res * 10 + ch - '0'; ch = getchar(); }
+		return *this;
+	}
+} cin;
+struct FastStreamOutput
+{
+	FastStreamOutput& operator<<(char x)
+	{
+		putchar(x);
+		return *this;
+	}
+	FastStreamOutput& operator<<(const char x[])
+	{
+		for (int i = 0; x[i]; ++i) putchar(x[i]);
+		return *this;
+	}
+	template<typename T>
+	FastStreamOutput& operator<<(const T &x)
+	{
+		static char buf[50]; int p = -1;
+		if (x == 0) putchar('0');
+		if (x < 0) { putchar('-'); x = -x; }
+		while (x) { buf[++p] = x % 10; x /= 10; }
+		while (~p) { putchar(buf[p] + '0'); ++p; }
+		return *this;
+	}
+} cout;
+
+int main(void)
+{
+	return 0;
+}
