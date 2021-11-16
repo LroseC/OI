fe955683cd515ac5b7f11148993a72a42d49aada
LroseC <1441276292@qq.com>
2021-11-16 13:06:58 +0800

    ...

---
+++ Luogu/3391/main.cpp
@@ -0,0 +1,24 @@
+#include <cctype>
+#include <cstdio>
+
+using namespace std;
+
+struct FSI
+{
+	template<typename T>
+	FSI& operator>>(T &res)
+	{
+		res = 0; T f = 1; char ch = getchar();
+		while (!isdigit(ch)) { if (ch == '-') f = -1; ch = getchar(); }
+		while (isdigit(ch)) { res = res * 10 + ch - '0'; ch = getchar(); }
+		res = res * f; return *this;
+	}
+} io;
+
+int n, m;
+
+int main(void)
+{
+	io >> n >> m;
+	return 0;
+}
