8881b94b11dd577449cab96cf83fe0285b9e93e7
LroseC <1441276292@qq.com>
2021-11-18 09:18:42 +0800

    ...

--- Luogu/3806/main.cpp
+++ Luogu/3806/main.cpp
@@ -1,5 +1,7 @@
+#include <vector>
 #include <cctype>
 #include <cstdio>
+#include <algorithm>
 
 using namespace std;
 
@@ -17,16 +19,76 @@ struct FSI
 
 const int N = 1e4 + 10;
 
-int n, m;
-int sz[N];
+struct Edge
+{
+	int v, w;
+	Edge(void) : v(0), w(0) {}
+	Edge(int v, int w) : v(v), w(w) {}
+};
+
+int n, m, allnode;
+int cnt[N];
+int sz[N], K[N];
+bool del[N], exist[N];
+vector<Edge> G[N];
 
 void get_root(int u, int fa, int &root, int &minsize)
 {
 	sz[u] = 1; int maxsize = 0;
 	for (auto e : G[u]) {
+		if (e.v == fa || del[e.v]) continue;
 		get_root(e.v, u, root, minsize);
 		sz[u] += sz[e.v];
-		maxsize = max(maxsize, sz[v]);
+		maxsize = max(maxsize, sz[e.v]);
+	}
+	maxsize = max(maxsize, allnode - sz[u]);
+	if (maxsize < minsize) {
+		root = u;
+		minsize = maxsize;
+	}
+}
+int stk[N], top;
+void get_dis(int u, int fa, int dis)
+{
+	stk[++top] = dis;
+	for (auto e : G[u]) {
+		if (e.v == fa || del[e.v]) continue;
+		get_dis(e.v, u, dis + e.w);
+	}
+}
+void calc(int u, int d, int f)
+{
+	top = 0;
+	get_dis(u, u, d);
+	sort(stk + 1, stk + 1 + top);
+	for (int id = 1; id <= m; ++id) {
+		if (exist[id]) continue;
+		for (int i = 1, j = top; i < j; ++i) {
+			while (i < j && stk[i] + stk[j] > K[id]) --j;
+			while (i < j && stk[i] + stk[j] == K[id]) {
+				--j;
+				cnt[id] += f;
+			}
+		}
+	}
+}
+void dfs(int u)
+{
+	del[u] = 1;
+	calc(u, 0, 1);
+	for (auto e : G[u]) {
+		if (del[e.v]) continue;
+		calc(e.v, e.w, -1);
+	}
+	for (int i = 1; i <= m; ++i) {
+		if (cnt[i]) exist[i] = 1;
+	}
+	for (auto e : G[u]) {
+		if (del[e.v]) continue;
+		allnode = sz[e.v];
+		int root, minsize = 0x3f3f3f3f;
+		get_root(e.v, u, root, minsize);
+		dfs(root);
 	}
 }
 
@@ -39,13 +101,15 @@ int main(void)
 		G[v].emplace_back(Edge(u, w));
 	}
 	for (int i = 1; i <= m; ++i) {
-		io >> K;
-		int ans;
-		allnode = n;
-		int root, minsize = 0x3f3f3f3f;
-		get_root(1, 1, root, minsize);
-		dfs(root);
-		puts(ans ? "AYE" : "NAY");
+		io >> K[i];
+	}
+	allnode = n;
+	int root, minsize = 0x3f3f3f3f;
+	get_root(1, 1, root, minsize);
+	dfs(root);
+	for (int i = 1; i <= m; ++i) {
+		if (exist[i]) puts("AYE");
+		else puts("NAY");
 	}
 	return 0;
 }
