1b55d55c1ede65370507738d0bea55fcf0b51a1e
LroseC <1441276292@qq.com>
2021-11-28 19:53:35 +0800

    ...

---
+++ Luogu/3572/main.cpp
@@ -0,0 +1,25 @@
+#include <cctype>
+#include <cstdio>
+
+using namespace std;
+
+struct FSI
+{
+	template<typename T>
+	FSI& operator>>(T &res)
+	{
+		res = 0; T f = 1; char ch = getchar();
+		while (!isdigit(ch)) { if (ch == '-') f = -1; ch = getchar(); }
+		while (isdigit(ch)) { res = res * 10 + ch - '0'; ch = getchar(); }
+		res = res * f; return *this;
+	}
+} io;
+
+int n, K;
+int h[N], f[N];
+
+int main(void)
+{
+	io >> n;
+	for (int i = 1; i <= n; ++i) io >> h[i];
+}
--- Luogu/New.sh
+++ Luogu/New.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-read last < ./New
-git add ./$last
-git commit -m "Luogu_$last"
 read name
-echo $name > New
 mkdir $name
 touch ./$name/main.cpp
-cd $name
