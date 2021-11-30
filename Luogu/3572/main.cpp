cb83c104971188b739e147927d6112b9729dcf55
LroseC <1441276292@qq.com>
2021-11-30 20:56:34 +0800

    ...

--- Luogu/3572/main.cpp
+++ Luogu/3572/main.cpp
@@ -15,11 +15,33 @@ struct FSI
 	}
 } io;
 
+const int N = 1e6 + 10;
+
 int n, K;
 int h[N], f[N];
+int q[N];
 
 int main(void)
 {
 	io >> n;
 	for (int i = 1; i <= n; ++i) io >> h[i];
+	int Q; io >> Q;
+	while (Q--) {
+		int K; io >> K;
+		q[0] = 1;
+		int hh = 0, tt = 0;
+		for (int i = 2; i <= n; ++i) {
+			while (hh <= tt && i - q[hh] > K) ++hh;
+			f[i] = f[q[hh]] + (h[i] >= h[q[hh]]);
+			auto cmp = [](int a, int b)
+			{
+				if (f[a] != f[b]) return f[a] < f[b];
+				return h[a] >= h[b];
+			};
+			while (hh <= tt && cmp(i, q[tt])) --tt;
+			q[++tt] = i;
+		}
+		printf("%d\n", f[n]);
+	}
+	return 0;
 }
