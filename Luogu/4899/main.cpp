b120a8731ed2e42d100d134f830220038d1d02f3
LroseC <1441276292@qq.com>
2022-01-05 17:27:45 +0800

    ...

--- Luogu/4899/main.cpp
+++ Luogu/4899/main.cpp
@@ -67,22 +67,20 @@ struct Kruskal
 {
 	int type;
 	int nodecnt;
-	int L[N << 1], R[N << 1], idx;
-	int fa[N << 1][LOG];
-	int nums[N << 1], w[N << 1];
-	vector<int> K[N << 1];
+	int L[N], R[N], idx;
+	int fa[N][LOG];
+	int nums[N];
+	vector<int> K[N];
 
 	void dfs(int u)
 	{
 		for (int k = 1; k < LOG; ++k)
 			fa[u][k] = fa[fa[u][k - 1]][k - 1];
 
-		if (!K[u].size()) {
-			++idx;
-			L[u] = R[u] = idx;
-			nums[idx] = u;
-			return;
-		}
+		++idx;
+		L[u] = R[u] = idx;
+		nums[idx] = u;
+
 		for (int v : K[u]) {
 			fa[v][0] = u;
 			dfs(v);
@@ -93,41 +91,55 @@ struct Kruskal
 	void build(int op)
 	{
 		type = op;
-		UF::init(n * 2);
+		UF::init(n);
 		memset(L, 0x3f, sizeof L);
-		for (int i = 1; i <= n; ++i) w[i] = i;
 		if (type == 1) {
-			//root is the max;
+			//root is max;
 			for (int u = 1; u <= n; ++u)
 				for (int v : G[u])
 					if (v < u) {
 						int fu = UF::find(u), fv = UF::find(v);
+						if (fu == fv) continue;
+						UF::merge(fv, fu);
+						K[fu].emplace_back(fv);
 					}
 		}
 		else {
+			//root is min;
 			for (int u = n; u >= 1; --u)
 				for (int v : G[u])
 					if (v > u) {
 						int fu = UF::find(u), fv = UF::find(v);
 						if (fu == fv) continue;
-						UF::merge(fu, fv);
-						K[fv].emplace_back(fu);
+						UF::merge(fv, fu);
+						K[fu].emplace_back(fv);
 					}
 		}
-		dfs(type == 1 ? 1 : n);
+		dfs(type == 1 ? n : 1);
+	}
+	void print(int u)
+	{
+		printf("u = %d\n", u);
+		for (int v : K[u]) print(v);
 	}
 	pair<int, int> query(int u, int lim)
 	{
 		if (type == 1) {
 			for (int k = LOG - 1; k >= 0; --k)
-				if (fa[u][k] && fa[u][k] >= lim)
+				if (fa[u][k] && fa[u][k] <= lim) {
 					u = fa[u][k];
+				}
 		}
 		else {
 			for (int k = LOG - 1; k >= 0; --k)
-				if (fa[u][k] && fa[u][k] <= lim)
+				if (fa[u][k] && fa[u][k] >= lim) {
 					u = fa[u][k];
+				}
 		}
+		puts("begin");
+		printf("type = %d, lim = %d\n", type, lim);
+		print(u);
+		puts("end");
 		return make_pair(L[u], R[u]);
 	}
 } TK_Human, TK_Monster;
@@ -140,6 +152,12 @@ void build_SGT(void)
 		TREE[i] = pos[TK_Monster.nums[i]];
 		root[i] = SGT::add(root[i - 1], TREE[i]);
 	}
+	puts("");
+	for (int i = 1; i <= n; ++i) printf("%d ", TK_Human.nums[i]);
+	puts("");
+	for (int i = 1; i <= n; ++i) printf("%d ", TK_Monster.nums[i]);
+	puts("");
+	puts("end");
 }
 
 bool query(int Human, int Monster, int LimHuman, int LimMoster)
@@ -162,12 +180,12 @@ int main(void)
 		G[u].emplace_back(v);
 		G[v].emplace_back(u);
 	}
-	TK_Human.build(1);
-	TK_Monster.build(0);
+	TK_Human.build(0);
+	TK_Monster.build(1);
 	build_SGT();
 	for (int i = 1; i <= q; ++i) {
 		int S, T, L, R; cin >> S >>T >> L >> R;
-		++S, ++T;
+		++S, ++T, ++L, ++R;
 		printf("%d\n", (int)query(S, T, L, R));
 	}
 	return 0;
