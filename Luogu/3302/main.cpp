9c83684ca08c70c318fe93176cd13cb38bc8ae12
LroseC <1441276292@qq.com>
2022-01-05 23:05:45 +0800

    ...

---
+++ Luogu/3302/main.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int CASEID;
+int n, m, q;
+
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin >> CASEID;
+	cin >> n >> m >> q;
+	for (int i = 1; i <= n; ++i) cin >> w[i];
+	return 0;
+}
