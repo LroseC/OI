1092218cae7cba974daa63152627dae323be6de9
LroseC <1441276292@qq.com>
2022-01-10 23:25:42 +0800

    ...

--- Luogu/5445/std.cpp
+++ Luogu/5445/std.cpp
@@ -1,36 +1,12 @@
 #include <set>
-#include <cctype>
 #include <cstdio>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 const int N = 3e5 + 10;
 inline int lowbit(int x) { return x & -x; }
-#define getchar() (p1==p2&&(p2=(p1=buf)+fread(buf,1,1<<21,stdin),p1==p2)?EOF:*p1++)
-static char buf[1<<21],*p1=buf,*p2=buf;
-
-struct FSI
-{
-	template<typename T>
-	FSI& operator>>(T &res)
-	{
-		res = 0; T f = 1; char ch = getchar();
-		while (!isdigit(ch)) { if (ch == '-') f = -1; ch = getchar(); }
-		while (isdigit(ch)) { res = res * 10 + ch - '0'; ch = getchar(); }
-		res = res * f; return *this;
-	}
-} cin;
-
-template<typename T>
-void write(T x)
-{
-	static char buf[12]; int p = -1;
-	if (!x) putchar('0');
-	if (x < 0) putchar('-'), x = -x;
-	while (x) buf[++p] = x % 10, x /= 10;
-	while (~p) putchar(buf[p] + '0'), --p;
-	putchar('\n');
-}
 
 int n, m;
 set<int> S;
@@ -90,24 +66,22 @@ void addMatrix(int x1, int x2, int y1, int y2, int val)
 	add(x2 + 1, y1, y2, -val);
 }
 
-int main(void)
+signed main(void)
 {
+	ios::sync_with_stdio(0);
 	cin >> n >> m;
 	for (int i = 0; i <= n + 1; ++i)
 		S.insert(i);
-	for (int i = 1; i <= n; ++i) {
-		do a[i] = getchar();
-		while (isspace(a[i]));
+	cin >> a + 1;
+	for (int i = 1; i <= n; ++i)
 		if (a[i] == '1') {
 			S.erase(i);
 			road[i] = 1;
 		}
-	}
 	for (int i = 1; i <= m; ++i) {
-		char op = getchar();
-		while (isspace(op))
-			op = getchar();
-		if (op == 't') {
+		static char op[10];
+		cin >> op;
+		if (op[0] == 't') {
 			int pos; cin >> pos;
 			road[pos] ^= 1;
 			if (road[pos]) {
@@ -132,16 +106,8 @@ int main(void)
 			cin >> u >> v;
 			auto it = S.lower_bound(u);
 			int right_bound = *it;
-			write(query(u, v) + (v <= right_bound) * i);
+			printf("%d\n", query(u, v) + (v <= right_bound) * i);
 		}
 	}
 	return 0;
 }
-//7 -> 11, 98 -> 101
-/*
-   一个奇怪的做法: 不用二维差分, 而是树状数组用差分, 线段树直接区间加即可.
-   应该还是能过? 
-*/
-/*
-   过了, 并且卡了个大常, 卡进卡常榜前十, 可喜可贺.
-*/
