fd66d7a70f003504b7e8e6dc88293facf49f8376
LroseC <1441276292@qq.com>
2022-01-17 19:12:58 +0800

    ...

--- Luogu/4067/main.cpp
+++ Luogu/4067/main.cpp
@@ -4,8 +4,11 @@
 
 using namespace std;
 
+#define int long long
+
 int mod;
 
+using LL = long long;
 using PII = pair<int, int>;
 
 void add(int &x, int y)
@@ -14,7 +17,7 @@ void add(int &x, int y)
 	if (x >= mod) x -= mod;
 }
 
-int n, m, k;
+LL n, m, k;
 int nlen, mlen, klen, maxlen;
 bool vis[70][2][2][2];
 PII f[70][2][2][2];
@@ -33,10 +36,15 @@ PII dp(int pos, bool n1, bool m1, bool k1)
 			add(f[pos][n1][m1][k1].first, nw.first);
 			add(f[pos][n1][m1][k1].second, ((1ll << pos) * (nn ^ mm) % mod * nw.first % mod + nw.second) % mod);
 		}
+//	printf("pos = %d, n1 = %d, m1 = %d, k1 = %d\n", pos, n1, m1, k1);
+//	printf("np = %d, mp = %d, kp = %d\n", np, mp, kp);
+//	auto res = f[pos][n1][m1][k1];
+//	printf("first = %d, second = %d\n", res.first, res.second);
+//	puts("");
 	return f[pos][n1][m1][k1];
 }
 
-int main(void)
+signed main(void)
 {
 	ios::sync_with_stdio(0);
 	int T;
@@ -71,8 +79,8 @@ int main(void)
 		}
 		maxlen = max(maxlen, klen);
 
-		PII ans = dp(maxlen, 0, 0, 0);
-		printf("%d\n", ans.second);
+		PII ans = dp(maxlen - 1, 1, 1, 1);
+		printf("%d\n", (1ll * ans.second - 1ll * k % mod * ans.first % mod + mod) % mod);
 	}
 	return 0;
 }
