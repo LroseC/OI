9c8448297834eb8e43c85b41b499222f63178fbb
LroseC <1441276292@qq.com>
2022-01-21 11:03:41 +0800

    ...

--- Luogu/5772/main.cpp
+++ Luogu/5772/main.cpp
@@ -59,91 +59,71 @@ struct Matrix
 
 const int N = 60;
 
-int n, k;
-char str[N];
+Matrix qpow(Matrix base, int k);
+int get(int pos, int state, int array);
 
-void printin2(int x)
-{
-	for (int i = 0; i < n; ++i)
-		printf("%d", x >> i & 1);
-}
-void printin2p(int x)
-{
-	for (int i = 0; i <= n; ++i)
-		printf("%d", x >> i & 1);
-}
-Matrix qpow(Matrix base, int k)
-{
-	Matrix res(base.r, base.c, 1);
-	while (k) {
-		if (k >> 1 & 1) res = res * base;
-		k >>= 1;
-		base = base * base;
-	}
-	return res;
-}
-int Getcnt(int x)
-{
-	int res = 0;
-	while (x) {
-		++res;
-		x -= lowbit(x);
-	}
-	return res;
-}
-
-int get(int pos, int state, int array)
-{
-	/*
-	   get 函数返回处理到第 pos 位, 当前状态为 state, 如果数组为 array 时转移到的状态
-	   如果无法转移 (不合法) 返回 -1
-	   get 函数的复杂度为 O(n)
-   */
-	if (Getcnt(array) & 1) return -1;
-	int tostate = 0;
-	array = array << 1 | (str[pos] - '0');
-	for (int i = 0; i < n; ++i) {
-		int ai = array >> i & 1, ai1 = array >> i + 1 & 1;
-		if ((state >> i & 1) && ai < ai1)
-			return -1;
-		tostate |= ((state >> i & 1) && ai == ai1) << i;
-	}
-	return tostate;
-}
+int n, k, len;
+char str[60];
+int f[60][1 << 8];
+int bitcnt[1 << 8];
 
 int main(void)
 {
 	IO >> n >> k;
 	scanf("%s", str + 1);
-	int len = strlen(str + 1);
-	Matrix base(1 << n, 1 << n, 1);
+	len = strlen(str + 1);
+	for (int i = 1; i <= len; ++i)
+		str[i] -= '0';
+	for (int i = 1; i < 1 << 8; ++i)
+		bitcnt[i] = bitcnt[i >> 1] + (i & 1);
 
-	Matrix res(1, 1 << n);
-	for (int i = 0; i < n; ++i)
-		res[0][(1 << n) - 1 ^ (1 << i)] = 1;
-
-	for (int pos = 1; pos <= len; ++pos) {
-		Matrix tmpMa(1 << n, 1 << n);
-		for (int state = 0; state < 1 << n; ++state)
-			for (int array = 0; array < 1 << n; ++array) {
-				int tmp = get(pos, state, array);
-				if (tmp == -1) continue;
-
-				//state -> tmp
-				++tmpMa[state][tmp];
+	Matrix trans(1 << n, 1 << n);
+	for (int st = 0; st < 1 << n; ++st) {
+		memset(f, 0, sizeof f);
+		f[0][st] = 1;
+		for (int pos = 1; pos <= len; ++pos) {
+			for (int state = 0; state < 1 << n; ++state) {
+				if (!f[pos - 1][state]) continue;
+				for (int array = 0; array < 1 << n; ++array) {
+					int tmp = get(pos, state, array);
+					if (tmp == -1) continue;
+					f[pos][tmp] = (f[pos][tmp] + f[pos - 1][state]) % mod;
+				}
 			}
-		base = base * tmpMa;
-		if (pos != 1) res = res * tmpMa;
+		}
+		for (int i = 0; i < 1 << n; ++i)
+			trans[st][i] = f[len][i];
 	}
-	base = qpow(base, k - 1);
-	res = res * base;
+	trans = qpow(trans, k);
+	Matrix res(1, 1 << n);
+	res[0][(1 << n) - 1] = 1;
+	res = res * trans;
 	printf("%d\n", res[0][0]);
 	return 0;
 }
-/*
-   f[i][state] 表示考虑前 i 位, 状态为 state 时的方案数
-   把 f[i] 整个数组视作维数为 2^n 的一个向量
-   这时可以构造一个转移矩阵 M, 令 f[i] = f[i - 1] * M;
-   然后把一个循环节的转移矩阵乘在一起, 最终的转移矩阵就是这个乘积矩阵的幂
-   快速幂解决即可.
-*/
+
+int get(int pos, int state, int array)
+{
+	if (bitcnt[array] & 1) return -1;
+	int tostate = 0;
+	array = (array << 1) | str[pos];
+	for (int i = 0; i < n; ++i) {
+		int ai = array >> i & 1, ai1 = array >> (i + 1) & 1;
+		if (state >> i & 1) {
+			if (ai < ai1) return -1;
+			if (ai == ai1)
+				tostate |= 1 << i;
+		}
+	}
+	return tostate;
+}
+Matrix qpow(Matrix base, int k)
+{
+	Matrix res(base.r, base.c, 1);
+	while (k) {
+		if (k & 1) res = res * base;
+		k >>= 1;
+		base = base * base;
+	}
+	return res;
+}
