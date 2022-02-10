775a7e6db654b83047d238a537e96aafa8380249
LroseC <1441276292@qq.com>
2022-02-10 15:20:04 +0800

    ...

--- Luogu/3809/main.cpp
+++ Luogu/3809/main.cpp
@@ -1,6 +1,7 @@
 #include <cctype>
 #include <cstdio>
 #include <cstring>
+#include <algorithm>
 
 using namespace std;
 
@@ -13,17 +14,32 @@ int c[N], x[N], y[N];
 
 void get_sa(void)
 {
-	m = 133;
-	for (int i = 1; i <= n; ++i)
-		++c[x[i] = str[i]];
-	for (int i = 2; i <= m; ++i)
-		c[i] += c[i - 1];
-	for (int i = n; i >= 1; --i)
-		sa[c[x[i]]--] = i;
+	m = 200;
+	for (int i = 1; i <= n; ++i) ++c[x[i] = str[i]];
+	for (int i = 2; i <= m; ++i) c[i] += c[i - 1];
+	for (int i = n; i >= 1; --i) sa[c[x[i]]--] = i;
 
 	for (int k = 1; k <= n; k <<= 1) {
 		int num = 0;
-		for (int i = n - k + 1; i <= n; ++i)
+		for (int i = n - k + 1; i <= n; ++i) y[++num] = i;
+		for (int i = 1; i <= n; ++i)
+			if (sa[i] > k)
+				y[++num] = sa[i] - k;
+
+		for (int i = 1; i <= m; ++i) c[i] = 0;
+		for (int i = 1; i <= n; ++i) ++c[x[i]];
+		for (int i = 2; i <= m; ++i) c[i] += c[i - 1];
+		for (int i = n; i >= 1; --i) sa[c[x[y[i]]]--] = y[i], y[i] = 0;
+
+		swap(x, y);
+		num = 0;
+		for (int i = 1; i <= n; ++i)
+			x[sa[i]] = y[sa[i]] == y[sa[i - 1]] && y[sa[i] + k] == y[sa[i - 1] + k] ? num : ++num;
+		if (num == n) break;
+		m = num;
+	}
+}
+
 
 int main(void)
 {
