b418c241f8324a97b1b729f8263b9f8c9181cb9e
LroseC <1441276292@qq.com>
2022-02-17 08:32:18 +0800

    ...

---
+++ Luogu/8148/main.cpp
@@ -0,0 +1,36 @@
+#include <set>
+#include <vector>
+#include <cstdio>
+
+using namespace std;
+
+const int N = 1e5 + 10;
+
+int n;
+multiset<int> S;
+int ans[N];
+int sum[N];
+
+int main(void)
+{
+	scanf("%d", &n);
+	int m = n * (n + 1) / 2;
+	for (int i = 1; i <= m; ++i) {
+		int x; scanf("%d", &x);
+		if (x <= 1e5)
+			S.insert(x);
+	}
+	for (int i = 1; i <= n; ++i) {
+		sum[i] = ans[i] = *S.begin();
+		sum[i] += sum[i - 1];
+		for (int j = 1; j <= i; ++j) {
+			if (sum[i] - sum[j - 1] > 1e5) continue;
+			auto it = S.find(sum[i] - sum[j - 1]);
+			S.erase(it);
+		}
+	}
+	for (int i = 1; i <= n; ++i)
+		printf("%d ", ans[i]);
+	puts("");
+	return 0;
+}
