3b91f3aea5df3980c5a703d999d2222b560dec9a
LroseC <1441276292@qq.com>
2022-02-26 15:18:53 +0800

    ...

---
+++ Luogu/Contest_2022-2-26/A/main.cpp
@@ -0,0 +1,28 @@
+#include <cctype>
+#include <cstdio>
+
+using namespace std;
+
+struct FSI
+{
+	template<typename T>
+	FSI& operator>>(T &res)
+	{
+		res = 0; T f = 1; char ch = getchar();
+		while (!isdigit(ch)) { if (ch == '-') f = -1; ch = getchar(); }
+		while (isdigit(ch)) { res = res * 10 + ch - '0'; ch = getchar(); }
+		res = res * f; return *this;
+	}
+} read;
+
+int prime[N];
+bool vis[N];
+
+void shai(void)
+{
+	for (int i = 2; i < N; ++i)
+
+int main(void)
+{
+	return 0;
+}
