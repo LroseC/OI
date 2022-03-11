610344940a6ef8698de233e588aadc66d17ba4b0
LroseC <1441276292@qq.com>
2022-03-11 20:16:41 +0800

    ...

--- Luogu/AT1219/main.cpp
+++ Luogu/AT1219/main.cpp
@@ -1,5 +1,8 @@
+#include <vector>
 #include <cctype>
 #include <cstdio>
+#include <cstring>
+#include <algorithm>
 
 struct FSI
 {
@@ -14,20 +17,51 @@ struct FSI
 } read;
 
 using i64 = long long;
-const int N = 1e5 + 10;
+const int N = 1e5 + 10, B = 316;
+
+struct Query
+{
+	int id, l, r;
+	bool operator<(const Query &other) const
+	{
+		if (l / B != other.l / B) return l < other.l;
+		return r < other.r;
+	}
+};
 
 int n, m;
-i64 cnt[N];
+int type[N];
+i64 cnt[N], ans[N];
+Query q[N];
+std::vector<int> nums;
+
+void Add(int pos, i64 &res)
+{
+	++cnt[type[pos]];
+	res = std::max(res, 1ll * cnt[type[pos]] * nums[type[pos]]);
+}
+void Dec(int pos)
+{
+	--cnt[type[pos]];
+}
 
 int main(void)
 {
 	read >> n >> m;
-	for (int i = 1; i <= n; ++i)
+	nums.resize(n);
+	for (int i = 1; i <= n; ++i) {
 		read >> type[i];
+		nums[i - 1] = type[i];
+	}
 	for (int i = 1; i <= m; ++i) {
 		q[i].id = i;
 		read >> q[i].l >> q[i].r;
 	}
+	std::sort(nums.begin(), nums.end());
+	nums.erase(unique(nums.begin(), nums.end()), nums.end());
+	for (int i = 1; i <= n; ++i)
+		type[i] = std::lower_bound(nums.begin(), nums.end(), type[i]) - nums.begin();
+
 	std::sort(q + 1, q + 1 + m);
 
 	int L, R; i64 res;
@@ -36,6 +70,7 @@ int main(void)
 			res = 0;
 			L = q[i].l / B * B + B;
 			R = L - 1;
+			memset(cnt, 0, sizeof cnt);
 		}
 		if (q[i].l / B == q[i].r / B) {
 			i64 res = 0;
@@ -48,10 +83,13 @@ int main(void)
 		else {
 			while (R < q[i].r) Add(++R, res);
 			i64 tmp = res;
-			for (int j = L; j >= q[i].l; --j) Add(j, res);
+			for (int j = L - 1; j >= q[i].l; --j) Add(j, res);
 			ans[q[i].id] = res;
+			for (int j = L - 1; j >= q[i].l; --j) Dec(j);
 			res = tmp;
 		}
 	}
+	for (int i = 1; i <= m; ++i)
+		printf("%lld\n", ans[i]);
 	return 0;
 }
