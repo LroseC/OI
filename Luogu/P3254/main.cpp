b642b2ee6843d2f053e4401144ab9d1e96ac2f43
LroseC <1441276292@qq.com>
2022-03-28 19:58:58 +0800

    ...

---
+++ Luogu/P3254/main.cpp
@@ -0,0 +1,107 @@
+#include <queue>
+#include <cctype>
+#include <cstdio>
+#include <cstring>
+#include <climits>
+#include <algorithm>
+
+struct FSI
+{
+	template<typename T>
+	FSI& operator>>(T &res)
+	{
+		res = 0; T f = 1; char ch = getchar();
+		while (!isdigit(ch)) { if (ch == '-') f = -1; ch = getchar(); }
+		while (isdigit(ch)) { res = res * 10 + ch - '0'; ch = getchar(); }
+		res = res * f; return *this;
+	}
+} read;
+
+const int N = 1e4 + 10, M = 1e5 + 10;
+
+int m, n, S = N - 1, T = N - 2;
+int idx = 1, head[N], nex[M], to[M], fl[M];
+
+void addEdge(int u, int v, int f)
+{
+	nex[++idx] = head[u];
+	head[u] = idx;
+	to[idx] = v, fl[idx] = f;
+}
+void addFlow(int u, int v, int f)
+{
+	addEdge(u, v, f);
+	addEdge(v, u, 0);
+}
+namespace Dinic
+{
+	int d[N], now[N];
+
+	bool BFS(void)
+	{
+		std::queue<int> q;
+		std::memset(d, 0, sizeof d);
+		std::memcpy(now, head, sizeof head);
+		d[S] = 1;
+		q.emplace(S);
+		while (q.size()) {
+			int u = q.front(); q.pop();
+			for (int e = head[u]; e; e = nex[e])
+				if (fl[e] && !d[to[e]]) {
+					d[to[e]] = d[u] + 1;
+					if (to[e] == T) return 1;
+					q.emplace(to[e]);
+				}
+		}
+		return 0;
+	}
+	int dfs(int u, int flow)
+	{
+		if (u == T)
+			return flow;
+		int rest = flow;
+		for (int e = now[u]; rest && e; e = nex[e])
+			if (fl[e] && d[to[e]] == d[u] + 1) {
+				now[u] = e;
+				int tmp = dfs(to[e], std::min(rest, fl[e]));
+				if (!tmp) d[to[e]] = 0;
+				rest -= tmp;
+				fl[e] -= tmp;
+				fl[e ^ 1] += tmp;
+			}
+		return flow - rest;
+	}
+	int main(void)
+	{
+		int res = 0;
+		while (BFS())
+			res += dfs(S, INT_MAX);
+		return res;
+	}
+}
+
+int main(void)
+{
+	read >> m >> n;
+	int tot = 0;
+	for (int i = 1; i <= m; ++i) {
+		int x; read >> x;
+		tot += x;
+		addFlow(S, i, x);
+		for (int j = 1; j <= n; ++j)
+			addFlow(i, j + m, 1);
+	}
+	for (int i = 1; i <= n; ++i) {
+		int x; read >> x;
+		addFlow(i + m, T, x);
+	}
+	if (Dinic::main() != tot)
+		puts("0");
+	else {
+		puts("1");
+		for (int i = 1; i <= m; ++i, puts(""))
+			for (int e = head[i]; e; e = nex[e])
+				if (to[e] != S && !fl[e]) printf("%d ", to[e] - m);
+	}
+	return 0;
+}
