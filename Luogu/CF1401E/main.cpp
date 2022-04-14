d03401addfd25bfae38be1c2485df0dc81bf13f3
LroseC <1441276292@qq.com>
2022-04-14 16:34:21 +0800

    ...

--- Luogu/CF1401E/main.cpp
+++ Luogu/CF1401E/main.cpp
@@ -1,5 +1,7 @@
+#include <vector>
 #include <cctype>
 #include <cstdio>
+#include <algorithm>
 
 struct FSI
 {
@@ -14,8 +16,51 @@ struct FSI
 } read;
 
 using i64 = long long;
+const int M = 1e6 + 10;
+inline int lowbit(int x) { return x & -x; }
+
+int n, m;
+std::vector< std::pair<int, int> > op[M], q[M];
+int tr[M];
+
+void add(int pos, int x)
+{
+	++pos;
+	for (int i = pos; i < M; i += lowbit(i))
+		tr[i] += x;
+}
+int query(int pos)
+{
+	++pos;
+	int res = 0;
+	for (int i = pos; i; i -= lowbit(i))
+		res += tr[i];
+	return res;
+}
 
 int main(void)
 {
+	read >> n >> m;
+	i64 res = 1;
+	for (int i = 1; i <= n; ++i) {
+		int pos, l, r;
+		read >> pos >> l >> r;
+		res += (l == 0 && r == 1e6);
+		op[l].emplace_back(pos, 1);
+		op[r + 1].emplace_back(pos, -1);
+	}
+	for (int i = 1; i <= m; ++i) {
+		int pos, l, r;
+		read >> pos >> l >> r;
+		res += (l == 0 && r == 1e6);
+		q[pos].emplace_back(std::make_pair(l, r));
+	}
+	for (int i = 0; i <= 1e6; ++i) {
+		for (auto t : op[i])
+			add(t.first, t.second);
+		for (auto t : q[i])
+			res += query(t.second) - query(t.first - 1);
+	}
+	printf("%lld\n", res);
 	return 0;
 }
