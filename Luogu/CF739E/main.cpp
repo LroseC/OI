376283253d98e98cf417b3caf15b0b416e19e1e0
LroseC <1441276292@qq.com>
2022-05-03 17:24:38 +0800

    ...

---
+++ Luogu/CF739E/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using i64 = long long;
+
+int main(void)
+{
+	std::cin >> n >> cnta >> cntb;
+	for (int i = 1; i <= n; ++i)
+		std::cin >> a[i];
+	for (int i = 1; i <= n; ++i)
+		std::cin >> b[i];
+	double l = -1e4, r = 1e4;
+	while (r - l > eps)
+	return 0;
+}
