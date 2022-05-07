6c505adaf52eca4b26006344ca4d29c6b015447e
LroseC <1441276292@qq.com>
2022-05-07 22:03:24 +0800

    ...

--- Luogu/CF888G/main.cpp
+++ Luogu/CF888G/main.cpp
@@ -1,56 +1,56 @@
 #include <cctype>
 #include <cstdio>
-#include <climits>
-#include <iostream>
-#include <algorithm>
 
-using namespace std;
-
-using LL = long long;
-const int N = 2e5 + 10;
-
-LL ans;
-int n;
-int A[N];
-int root, idx;
-int ch[N * 31][2], L[N * 31], R[N * 31];
-
-void insert(int &u, int val, int id, int dep)
+struct FSI
 {
-	if (!u) u = ++idx;
-	if (dep < 0) return;
-	int e = val >> dep & 1;
-	if (!L[u]) L[u] = id;
-	R[u] = id;
-	insert(ch[u][e], val, id, dep - 1);
-}
-int query(int u, int val, int dep)
-{
-	if (dep < 0) return 0;
-	int e = val >> dep & 1;
-	if (ch[u][e]) return query(ch[u][e], val, dep - 1);
-	else return query(ch[u][e ^ 1], val, dep - 1) | (1 << dep);
-}
-void dfs(int u, int dep)
-{
-	if (ch[u][0] && ch[u][1]) {
-		int res = INT_MAX;
-		for (int i = L[ch[u][0]]; i <= R[ch[u][0]]; ++i)
-			res = min(res, query(ch[u][1], A[i], dep - 1) | (1 << dep)); //!!!
-		ans += res;
+	template<typename T>
+	FSI& operator>>(T &res)
+	{
+		res = 0; T f = 1; char ch = getchar();
+		while (!isdigit(ch)) { if (ch == '-') f = -1; ch = getchar(); }
+		while (isdigit(ch)) { res = res * 10 + (ch - '0') * f; ch = getchar(); }
+		return *this;
 	}
-	if (ch[u][0]) dfs(ch[u][0], dep - 1);
-	if (ch[u][1]) dfs(ch[u][1], dep - 1);
+} read;
+
+using i64 = long long;
+
+int getCnt(void)
+{
+	int res = 0;
+	std::memset(vis, 0, sizeof vis);
+	for (int i = 1; i <= n; ++i) {
+		int u = find(i);
+		if (!vis[u]) {
+			++res;
+			vis[u] = 1;
+		}
+	}
 }
 
 int main(void)
 {
-	ios::sync_with_stdio(0);
-	cin >> n;
-	for (int i = 1; i <= n; ++i) cin >> A[i];
-	sort(A + 1, A + 1 + n);
-	for (int i = 1; i <= n; ++i) insert(root, A[i], i, 30);
-	dfs(root, 30);
-	printf("%lld\n", ans);
+	read >> n;
+	idx = n;
+	for (int i = 1; i <= n; ++i) {
+		read >> a[i];
+		fa[i] = i;
+		insert(0, a[i]);
+		insert(i, a[i]);
+		val[i].emplace_back(i);
+	}
+	i64 res = 0;
+	if (getCnt() != 1) {
+		for (auto t : edge) {
+			res += t.w;
+			int u = find(t.u), v = find(t.v);
+			if (sz[u] > sz[v])
+				std::swap(u, v);
+			sz[v] += sz[u];
+			fa[u] = v;
+			for (int x : val[u])
+				insert(v, a[x]);
+		}
+	}
 	return 0;
 }
