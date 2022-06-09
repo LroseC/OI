09a1b52dde6262d41c730477425bedc056075e7f
LroseC <1441276292@qq.com>
2022-06-09 16:44:55 +0800

    ...

--- AtCoder/Beginner_254/E/main.cpp
+++ AtCoder/Beginner_254/E/main.cpp
@@ -1,5 +1,8 @@
+#include <queue>
+#include <vector>
 #include <cctype>
 #include <cstdio>
+#include <cstring>
 
 struct FSI
 {
@@ -14,8 +17,51 @@ struct FSI
 } read;
 
 using i64 = long long;
+const int N = 2e5 + 10;
+
+int n, m;
+int dis[N];
+int stk[N], top;
+std::vector<int> G[N];
+
+int bfs(int u, int lim)
+{
+	std::queue<int> q;
+	dis[u] = 0;
+	q.emplace(u);
+	int res = 0;
+	while (q.size()) {
+		int u = q.front(); q.pop();
+		res += u;
+		for (int v : G[u])
+			if (dis[v] == -1 && dis[u] < lim) {
+				dis[v] = dis[u] + 1;
+				stk[++top] = v;
+				q.emplace(v);
+			}
+	}
+	dis[u] = -1;
+	while (top) {
+		dis[stk[top--]] = -1;
+	}
+	return res;
+}
 
 int main(void)
 {
+	std::memset(dis, -1, sizeof dis);
+	read >> n >> m;
+	for (int i = 1; i <= m; ++i) {
+		int u, v;
+		read >> u >> v;
+		G[u].emplace_back(v);
+		G[v].emplace_back(u);
+	}
+	int q; read >> q;
+	while (q--) {
+		int u, lim;
+		read >> u >> lim;
+		printf("%d\n", bfs(u, lim));
+	}
 	return 0;
 }
--- AtCoder/Beginner_254/F/main.cpp
+++ AtCoder/Beginner_254/F/main.cpp
@@ -1,5 +1,7 @@
+#include <vector>
 #include <cctype>
 #include <cstdio>
+#include <algorithm>
 
 struct FSI
 {
@@ -14,8 +16,61 @@ struct FSI
 } read;
 
 using i64 = long long;
+const int N = 2e5, LG = 18;
+
+struct stTable
+{
+	int size;
+	int st[LG][N], lg2[N];
+	void build(std::vector<int> &o)
+	{
+		size = o.size();
+		lg2[1] = 0;
+		for (int i = 2; i < size; ++i) {
+			lg2[i] = lg2[i >> 1] + 1;
+		}
+		for (int i = 0; i < size; ++i) {
+			st[0][i] = o[i];
+		}
+		for (int j = 1; j < LG; ++j) {
+			for (int i = 0; i + (1 << j) - 1 < size; ++i)
+				st[j][i] = std::__gcd(st[j - 1][i], st[j - 1][i + (1 << j - 1)]);
+		}
+	}
+	int query(int L, int R)
+	{
+		int o = lg2[R - L + 1];
+		return std::__gcd(st[o][L], st[o][R - (1 << o) + 1]);
+	}
+} sta, stb;
 
 int main(void)
 {
+	int n, m;
+	read >> n >> m;
+	std::vector<int> a(n), b(n), decA(n - 1), decB(n - 1);
+	for (int i = 0; i < n; ++i) {
+		read >> a[i];
+	}
+	for (int i = 0; i < n; ++i) {
+		read >> b[i];
+	}
+	for (int i = 0; i < n - 1; ++i) {
+		decA[i] = abs(a[i + 1] - a[i]);
+		decB[i] = abs(b[i + 1] - b[i]);
+	}
+	sta.build(decA);
+	stb.build(decB);
+	for (int i = 0; i < m; ++i) {
+		int l1, r1, l2, r2;
+		read >> l1 >> r1 >> l2 >> r2;
+		--l1, --r1, --l2, --r2;
+		int res = a[l1] + b[l2];
+		if (l1 < r1)
+			res = std::__gcd(res, sta.query(l1, r1 - 1));
+		if (l2 < r2)
+			res = std::__gcd(res, stb.query(l2, r2 - 1));
+		printf("%d\n", res);
+	}
 	return 0;
 }
