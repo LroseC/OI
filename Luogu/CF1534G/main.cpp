2a286c470403cf2b9f7d2ed6974e461b8359bed6
LroseC <1441276292@qq.com>
2022-07-08 14:55:32 +0800

    ...

--- Luogu/CF1534G/main.cpp
+++ Luogu/CF1534G/main.cpp
@@ -1,3 +1,4 @@
+#include <queue>
 #include <vector>
 #include <cctype>
 #include <cstdio>
@@ -18,9 +19,45 @@ struct FSI
 using i64 = long long;
 using vint = std::vector<int>;
 using PII = std::pair<int, int>;
+const int N = 1e6 + 10;
+
+int n;
+PII a[N];
 
 int main(void)
 {
 	//Think twice, code once.
+	read >> n;
+	for (int i = 1; i <= n; ++i) {
+		read >> a[i].first >> a[i].second;
+	}
+	auto cmp = [](const PII &a, const PII &b)
+	{ return a.first + a.second < b.first + b.second; };
+	std::sort(a + 1, a + 1 + n, cmp);
+	i64 ans = 0, tagR = 0;
+	std::priority_queue<i64> L;
+	std::priority_queue<i64, std::vector<i64>, std::greater<i64>> R;
+	L.emplace(a[1].first); R.emplace(a[1].first);
+	for (int i = 2; i <= n; ++i) {
+		tagR += a[i].first + a[i].second - a[i - 1].first - a[i - 1].second;
+		int left = L.top(), right = R.top() + tagR;
+		if (a[i].first <= left) {
+			L.emplace(a[i].first);
+			L.emplace(a[i].first);
+			ans += left - a[i].first;
+			L.pop(); R.emplace(left - tagR);
+		}
+		else if (a[i].first >= right) {
+			R.emplace(a[i].first - tagR);
+			R.emplace(a[i].first - tagR);
+			ans += a[i].first - right;
+			R.pop(); L.emplace(right);
+		}
+		else {
+			L.emplace(a[i].first);
+			R.emplace(a[i].first - tagR);
+		}
+	}
+	printf("%lld\n", ans);
 	return 0;
 }
