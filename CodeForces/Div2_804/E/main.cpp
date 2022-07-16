ef5c1403be3daed1fb1e8698a0466ff8edfa1c24
LroseC <1441276292@qq.com>
2022-07-16 16:59:06 +0800

    ...

--- CodeForces/Div2_804/E/main.cpp
+++ CodeForces/Div2_804/E/main.cpp
@@ -1,3 +1,4 @@
+#include <set>
 #include <vector>
 #include <climits>
 #include <iostream>
@@ -7,6 +8,9 @@
 using i64 = long long;
 using vint = std::vector<int>;
 using PII = std::pair<int, int>;
+const int M = 5e6 + 10;
+
+int v[M], prime[M], idx;
 
 void getPrime(void)
 {
@@ -20,9 +24,6 @@ void getPrime(void)
 			if (v[i] == prime[j]) break;
 		}
 	}
-	for (int i = 0; i < 10; ++i)
-		printf("%d ", prime[i]);
-	puts("");
 }
 
 int main(void)
@@ -34,35 +35,44 @@ int main(void)
 	while (tt--) {
 		int n, m; read >> n >> m;
 		vint a(n);
-		std::vector<vint> pos(m + 1);
-		std::vector<std::set<int>> S(n);
-		int maxx = 0;
+		std::vector<vint> pos(m + 2);
+		std::vector<vint> S(n), S2(n);
+		int maxx = 0, minx = m;
+		bool flag = 0;
 		for (int i = 0; i < n; ++i) {
 			read >> a[i];
+			if (a[i] == 1) flag = 1;
 			for (int x = a[i]; x != 1; x /= v[x]) {
 				S[i].emplace(v[x]);
-				pos[v[x]].emplace_back(i);
+				pos[v[x] + 1].emplace_back(i);
 				maxx = std::max(maxx, v[x]);
+				minx = std::min(minx, v[x]);
 			}
+		}
+		if (flag) {
+			std::cout << maxx - 1 << '\n';
+			continue;
 		}
 		int res = INT_MAX;
-		for (int minx = 0; minx <= m; ++minx) {
+		for (; minx <= m; ++minx) {
 			bool fail = 0;
 			for (auto p : pos[minx]) {
-				while (S[p].size() > 1 && S[p].front() < minx) {
-					auto it = S.begin();
+				while (S[p].size() > 1 && *S[p].begin() < minx) {
+					auto it = S[p].begin();
 					auto jt = std::next(it);
-					maxx = std::max(maxx, (*it) * (*jt));
-					pos[(*it) * (*jt)].emplace_back(p);
-					S.insert((*it) * (*jt));
-					S.erase(it);
-					S.erase(jt);
+					int v = (*it) * (*jt);
+					S[p].erase(it);
+					S[p].erase(jt);
+					maxx = std::max(maxx, v);
+					pos[v + 1].emplace_back(p);
+					S[p].insert(v);
 				}
-				if (S[p].front() < minx) fail = 1;
+				if (*S[p].begin() < minx) fail = 1;
 			}
 			if (fail) break;
-			res = std::min(maxx - minx);
+			res = std::min(res, maxx - minx);
 		}
+		std::cout << res << '\n';
 	}
 	return 0;
 }
