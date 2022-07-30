3724aa0d81006e8afe1e5c1ab132187889393365
LroseC <1441276292@qq.com>
2022-07-30 13:42:12 +0800

    ...

--- CodeForces/Div1_768/template.cpp
+++ CodeForces/Div1_768/template.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <algorithm>
 
+#define read std::cin
 using i32 = int;
 using i64 = long long;
 using vi32 = std::vector<i32>;
@@ -13,4 +14,7 @@ int main(void)
 {
 	//Think twice, code once.
 	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	std::cout.tie(nullptr);
+	return 0;
 }
