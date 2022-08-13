75f3b1ba6e8c4053be2be0f549355bc6459c0958
LroseC <1441276292@qq.com>
2022-08-13 16:43:17 +0800

    ...

--- Luogu/P7112/main.cpp
+++ Luogu/P7112/main.cpp
@@ -1,5 +1,7 @@
+#include <vector>
 #include <cctype>
 #include <cstdio>
+#include <cstdint>
 #include <algorithm>
 
 struct FSI
@@ -9,41 +11,49 @@ struct FSI
 	{
 		res = 0; T f = 1; char ch = getchar();
 		while (!isdigit(ch)) { if (ch == '-') f = -1; ch = getchar(); }
-		while (isdigit(ch)) { res = res * 10 + ch - '0'; ch = getchar(); }
-		res = res * f; return *this;
+		while (isdigit(ch)) { res = res * 10 + (ch - '0') * f; ch = getchar(); }
+		return *this;
 	}
 } read;
 
-using i64 = long long;
+using i32 = int32_t;
+using i64 = int64_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using vi32 = std::vector<i32>;
+using PII = std::pair<i32, i32>;
 const int N = 610;
 
-int n, mod;
-int a[N][N];
+i32 n, mod;
+i32 v[N][N];
 
 int main(void)
 {
+	//Think twice, code once.
 	read >> n >> mod;
-	for (int i = 1; i <= n; ++i)
-		for (int j = 1; j <= n; ++j)
-			read >> a[i][j];
-	int res = 1;
+	for (int i = 1; i <= n; ++i) {
+		for (int j = 1; j <= n; ++j) {
+			read >> v[i][j];
+		}
+	}
+	i32 res = 1;
 	for (int i = 1; i <= n; ++i) {
 		for (int j = i + 1; j <= n; ++j) {
-			while (a[i][i]) {
-				int tmp = a[j][i] / a[i][i];
-				res *= -1;
-				for (int k = 1; k <= n; ++k) {
-					std::swap(a[i][k], a[j][k]);
-					a[i][k] = ((a[i][k] - 1ll * tmp * a[j][k]) % mod + mod) % mod;
+			while (v[j][i]) {
+				//b, a % b <=> a, a - b * (a / b);
+				i32 div = v[i][i] / v[j][i];
+				for (int t = 1; t <= n; ++t) {
+					v[i][t] = (v[i][t] - 1ll * v[j][t] * div) % mod;
+				}
+				res = -res;
+				for (int t = 1; t <= n; ++t) {
+					std::swap(v[i][t], v[j][t]);
 				}
 			}
-			res *= -1;
-			for (int k = 1; k <= n; ++k)
-				std::swap(a[i][k], a[j][k]);
 		}
 	}
 	for (int i = 1; i <= n; ++i)
-		res = 1ll * res * a[i][i] % mod;
+		res = 1ll * res * v[i][i] % mod;
 	printf("%d\n", (res + mod) % mod);
 	return 0;
 }
